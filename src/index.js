77a4c6af51be295c3a90d228ccd7e42d823e31c1
saywandeusher <ongsaywan@hotmail.com>
2018-09-04 20:53:26 +0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ParallaxProvider } from 'react-scroll-parallax';
-
-ReactDOM.render(<ParallaxProvider><App /></ParallaxProvider>, document.getElementById('root'));
-
