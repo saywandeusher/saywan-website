c33f653a83c691379cfe0bed4769dd0ef873c5dc
saywandeusher <ongsaywan@hotmail.com>
2018-09-04 20:52:33 +0800

    complete responsiveness

--- src/components/introduction/intro.jsx
+++ src/components/introduction/intro.jsx
@@ -6,10 +6,10 @@ export default class Intro extends React.Component {
   render() {
     return (
       <div className="parallax-container center valign-wrapper">
-        <div className="container">
+        <div className="container"> 
           <div className="row">
             <div className="col s12">
-              <h2 className="white-text lighten-2">HI, I'M SAY WAN, A FRONT-END WEB DEVELOPER.</h2> 
+              <div className="white-text lighten-2 heading2">HI, I'M SAY WAN, A FRONT-END WEB DEVELOPER.</div> 
               <a href="#aboutme" className="waves-effect waves-light btn-large white lighten-2 black-text pulse">CHECK ME OUT</a>
             </div>
           </div>
